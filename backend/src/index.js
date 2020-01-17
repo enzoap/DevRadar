47a49b8aec989a1b73cca7aee14b020af2c4e2b9
Enzo Augusto <enzoap2009@hotmail.com>
2020-01-16 22:05:12 -0300

    Alterações para deploy no heroku.

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.DB_URL,{
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use("/", express.static(__dirname + "/../../web/dist"))
+app.use('/', express.static(__dirname + "/../../web/dist/index.html"))
 const port = process.env.PORT || 3333
 app.listen(port, function () {
 console.log(`Servidor executando em ${port}`)
