cd517d5f0fcb9bb9f0066fc40733a03a39bda398
Enzo Augusto <enzoap2009@hotmail.com>
2020-01-23 20:41:19 -0300

    Finalização do app mobile em React-Native com websocket.

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.DB_URL,{
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use('/', express.static(__dirname+ "/../../web/build"))
+app.use('/', express.static(__dirname + "/../../web/build"))
 const port = process.env.PORT || 3333
 server.listen(port, function () {
 console.log(`Servidor executando em ${port}`)
