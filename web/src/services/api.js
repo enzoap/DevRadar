ed002ef340ff72a111126e96742e46f3f9fb8d9c
Enzo Augusto <enzoap2009@hotmail.com>
2020-01-23 20:49:57 -0300

    Atualizando a página web.

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const api = axios.create({
-        baseURL: process.env.REACT_APP_API_URL || "http://localhost:3333"
+        baseURL: process.env.REACT_APP_API_URL
 })
 
 export default api
